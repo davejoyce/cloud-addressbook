6ac147eda2040dc62e3d3b26a1bf5379b91e4212
Dave Joyce <dave@osframework.org>
2018-11-03 17:38:48 -0400

    #1 Spring Initializr bootstrap artifacts

---
+++ src/main/java/io/github/davejoyce/cloudaddressbook/CloudAddressbookApplication.java
@@ -0,0 +1,12 @@
+package io.github.davejoyce.cloudaddressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAddressbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAddressbookApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/davejoyce/cloudaddressbook/CloudAddressbookApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.davejoyce.cloudaddressbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudAddressbookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
